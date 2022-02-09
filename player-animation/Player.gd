c442be33f7a3890e361219766f9169ee6c5aaa01
Nathan Eloe <powerofazure@gmail.com>
2022-02-09 09:46:35 -0600

    Add player animation tutorial (Feb 4,9)

---
+++ player-animation/Player.gd
@@ -0,0 +1,27 @@
+extends KinematicBody2D
+
+const ACCELERATION = 500
+const MAX_SPEED = 200
+
+var velocity = Vector2.ZERO
+var animPlayer
+
+onready var animTree = $AnimationTree
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	animPlayer = $AnimationPlayer
+	animTree.active = true
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	var input_vector = Vector2.ZERO
+	input_vector.x = Input.get_action_strength('ui_right') - Input.get_action_strength('ui_left')
+	input_vector.y = Input.get_action_strength('ui_down') - Input.get_action_strength('ui_up')
+	input_vector = input_vector.normalized()
+	velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
+	animTree.set('parameters/blend_position', input_vector)
+	
+func _physics_process(delta):
+	velocity = move_and_slide(velocity)
